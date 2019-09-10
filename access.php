c75d4ca9045c0a1409b6c0888255e8b6a59da283
Ruben Castillo <rcastillo@raivtech.com>
2019-09-10 11:51:27 -0600

    Se cambia Nav para usuarios generales

--- access.php
+++ access.php
@@ -1,31 +1,8 @@
 <?php
     include 'login/include/sesiones.php';
     include 'layout/header.php';
+    include 'layout/nav-user.php';
 ?>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="access.php"><i class="fas fa-tv"></i> <i class="fas fa-grip-lines-vertical"></i> <i class="fas fa-broadcast-tower"></i></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      
-      
-    
-    </ul>
-    <ul class="nav">
-      
-      <li class="btn">
-        <a class="btn btn-info" href="login/index.php?cerrar_sesion=true">
-          Cerrar Sesión
-        </a>
-      </li> 
-    
-    </ul>
-  </div>
-</nav>
 
 <div class="table-bootstrap table-responsive-xl">
 
--- layout/nav-user.php
+++ layout/nav-user.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><i class="fas fa-tv"></i> <i class="fas fa-grip-lines-vertical"></i> <i class="fas fa-broadcast-tower"></i></a>
+  <a class="navbar-brand" href="#">RADIO / TV</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
